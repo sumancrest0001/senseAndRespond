e0a1573c9dc5c934b16b60277ad03c202a68ab81
Suman Shrestha <suman.shrestha@napses.com>
2024-02-04 20:03:10 +0545

    show latest root comment at top

--- src/utils/comments.js
+++ src/utils/comments.js
@@ -2,30 +2,24 @@ import { cloneDeep } from "lodash";
 
 const addCommentToCorrectParent = (parent, parentId, comment) => {
     const clonedParent = cloneDeep(parent);
-    // if it's a parent comment then add it to root level otherwise add to matching comment replies
-    if(!parentId) {
-        clonedParent.push(comment);
-
+    if(parent.id === parentId) {
+        clonedParent.replies = [comment, ...clonedParent.replies];
         return clonedParent;
-    }
-
-    if(parent.id === parentId || !parentId) {
-        parent.replies = [comment, ...parent.replies];
-        return parent;
     }
     let latestParent = [];
     // recursively go through the replies
-    latestParent = parent.replies.map(reply => addCommentToCorrectParent(reply, parentId, comment));
-    return {...parent, replies: latestParent}
+    latestParent = clonedParent.replies.map(reply => addCommentToCorrectParent(reply, parentId, comment));
+    return {...clonedParent, replies: latestParent}
     
 }
 
 export const addComment = (parent, parentId, comment) => {
     const clonedParent = cloneDeep(parent);
+    // if it's a parent comment then add it to root level otherwise add to matching comment replies
 
     if(!parentId) {
         clonedParent.push(comment);
-        return(clonedParent);
+        return([comment, ...clonedParent]);
     }
     const updatedParent = clonedParent.map(item => addCommentToCorrectParent(item, parentId, comment));
     return updatedParent;
